1ecddeaf9a3e1f94be14e66aa556baf19fa1d7fd
Bapparaj sk <bapparajsk97@gmail.com>
2025-05-09 20:17:57 +0530

    refactor(ui): Adjust UserList layout for employee view
    This commit refactors the UserList and UserListNav components
    to adjust their layout based on whether the view is displaying
    employees or regular users. This involves modifying the width
    of the left and right sections to provide better visual balance
    when displaying employee-specific information. The `cn` utility
    is used for conditional class application.

--- src/components/user/UserList.tsx
+++ src/components/user/UserList.tsx
@@ -3,6 +3,7 @@ import {RenderUserList} from "@/components/user/RenderUserList";
 import {UserListNav} from "@/components/user/UserListNav";
 import {UserListProps} from "&/components/user/userList";
 import {EmployeeDeletes, EmployeeList} from "@/components/employee";
+import {cn} from "@heroui/react";
 
 export const UserList = ({ isEmployee }: UserListProps) => {
     return (
@@ -11,10 +12,10 @@ export const UserList = ({ isEmployee }: UserListProps) => {
                 <UserListNav isEmployee={isEmployee}/>
             </div>
             <div className={"w-full h-[93%] flex"}>
-                <div className={"w-2/3 h-full"}>
+                <div className={cn("h-full", isEmployee ? "w-3/5" : "w-2/3")}>
                     {isEmployee ? <EmployeeList/> : <RenderUserList/>}
                 </div>
-                <div className={"w-1/3 h-full border-l border-gray-600 py-2 font-semibold space-y-2"}>
+                <div className={cn("h-full border-l border-gray-600 py-2 font-semibold space-y-2", isEmployee? "w-2/5" : "w-1/3")}>
                     <div className={"w-full h-full px-3"}>
                         <div className={"w-full h-full border border-gray-600 rounded-xl overflow-hidden"}>
                             {isEmployee ? <EmployeeDeletes/> : <RenderUser/>}
--- src/components/user/UserListNav.tsx
+++ src/components/user/UserListNav.tsx
@@ -17,6 +17,7 @@ import {CardTab} from "@/components/ui/CardTab";
 import {TabDataType, UserListProps} from "&/components/user/userList";
 import {useEmpContext} from "@/context/empContext";
 import {FilterProps} from "&/components/ui/Filter";
+import {cn} from "@heroui/react";
 
 const UserstabData: TabDataType[] = [
     {
@@ -67,7 +68,7 @@ const employeeTabData: TabDataType[] = [
 export const UserListNav = ({ isEmployee }: UserListProps) => {
 
     const { setListTabKeyName, listTabKey, selectedUserId, userTab, setUserTabName } = useUserList();
-    const { fetchEmployees, selectedEmployeeId, selectEmployeeId, selectedEmployee, employeeTab, selectEmployeeTab } = useEmpContext();
+    const { selectedEmployee, employeeTab, selectEmployeeTab } = useEmpContext();
 
     const tabProps: FilterProps["TabsProps"] = isEmployee ? {
         defaultSelectedKey: "all",
@@ -89,7 +90,7 @@ export const UserListNav = ({ isEmployee }: UserListProps) => {
 
     return (
         <div className={"w-full h-full flex items-center border-b border-gray-600"}>
-            <div className={"w-2/3"}>
+            <div className={isEmployee ? "w-3/5" : "w-2/3"}>
                 <Filter
                     showBorder={false}
                     InputProps={{ placeholder: "Search Users..." }}
@@ -105,7 +106,7 @@ export const UserListNav = ({ isEmployee }: UserListProps) => {
                     }}
                 />
             </div>
-            <div className={"w-1/3 h-full border-l border-gray-600 py-2 font-semibold space-y-2"}>
+            <div className={cn("h-full border-l border-gray-600 py-2 font-semibold space-y-2", isEmployee ? "w-2/5" : "w-1/3")}>
                 <div className={"w-full h-full flex items-center"}>
                     <CardTab
                         isDisabled={isEmployee ? !selectedEmployee : !selectedUserId}
